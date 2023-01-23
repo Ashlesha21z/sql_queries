b9821a7c9aeb09b962354521686a2e6d73768bf3
Ashlesha21z <ashleshazambare@gmail.com>
2023-01-23 18:53:34 +0530

    [IMP]: Added two new scripts

---
+++ num_is_odd_or_even.py
@@ -0,0 +1,10 @@
+
+
+def is_even_odd(num):
+    if num%2==0:
+        print("{} is even number".format(num))
+    else:
+        print("{} is odd number".format(num))
+
+num=int(input("Enter the Number:"))
+is_even_odd(num)
